4980eab4184c8feb021624a90fbbc4df3c9dd0a9
Peter Preinesberger <peter.preines@gmail.com>
2024-04-12 11:31:27 +0200

    vm instructions defined

---
+++ VM/include/instr.hpp
@@ -0,0 +1,103 @@
+#ifndef _LIBTHEO_VM_INSTR_HPP_
+#define _LIBTHEO_VM_INSTR_HPP_
+
+namespace Theo {
+  enum class OpCode {
+    NOP,
+    HALT,
+    ADD_CONST,
+    JMP,
+    JMPC,
+    PREPARE_EXEC,
+    ARG,
+    EXEC,
+    RET
+  };
+
+  typedef int RegisterIndex, JumpOffset, Constant, ProgramIndex, RegisterCount;
+
+  struct Instruction {
+    OpCode op;
+
+    union {
+      struct {
+	RegisterIndex target;
+	RegisterIndex source;
+	Constant constant;
+      } add;
+      struct {
+	JumpOffset offset;
+      } jmp;
+      struct {
+	JumpOffset offset;
+	RegisterIndex source;
+      } jmpc;
+      struct {
+	RegisterCount count;
+      } prepare;
+      struct {
+	RegisterIndex target;
+	RegisterIndex source;
+      } arg;
+      struct {
+	RegisterIndex target;
+	ProgramIndex entry;
+      } exec;
+      struct {
+	RegisterIndex source;
+      } ret;
+    } parameters;
+
+    static Instruction Nop();
+    
+    static Instruction Halt();
+    
+    /**
+     * <target> := <source> + <constant>
+     */
+    static Instruction Add(RegisterIndex target, RegisterIndex source, Constant constant);
+
+    /**
+     * InstructionPointer := InstructionPointer + <offset>
+     */
+    static Instruction Jmp(JumpOffset offset);
+
+    /**
+     * IF <source> != 0 THEN InstructionPointer := InstructionPointer + <offset>
+     */
+    static Instruction JmpC(JumpOffset offset, RegisterIndex source);
+
+    /**
+     * Creates new stack frame after the current one with <count> registers
+     * and initializes those to zero;
+     */
+    static Instruction PrepareExec (RegisterCount count);
+
+    /**
+     * copies from register <source> of second to last stack frame
+     * to register <target> of last stack frame
+     */
+    static Instruction Arg(RegisterIndex target, RegisterIndex source);
+
+    /**
+     * InstructionPointer := <entry>;
+     * sets the return target of the current stack frame to register <target>
+     * of the second to last stack frame
+     * used for calling functions:
+     * PREPARE 3
+     * ARG 1 13
+     * ARG 2 14
+     * EXEC <entry>
+     */
+    static Instruction Exec(RegisterIndex target, ProgramIndex entry);
+
+    /**
+     * copies from register <source> of the current stack frame
+     * to the return register specified for the second to last stack frame
+     * and resets InstructionPointer to the return address
+     */
+    static Instruction Ret(RegisterIndex source);
+  };
+}
+
+#endif
---
+++ VM/src/instr.cpp
@@ -0,0 +1,93 @@
+#include "VM/include/instr.hpp"
+
+using namespace Theo;
+
+Instruction Instruction::Nop() {
+  return {
+    .op = OpCode::NOP
+  };
+}
+
+Instruction Instruction::Add(RegisterIndex target, RegisterIndex source, Constant constant){
+  return {
+    .op = OpCode::ADD_CONST,
+    .parameters = {
+      .add = {
+	.target = target,
+	.source = source,
+	.constant = constant
+      }
+    }
+  };
+}
+
+Instruction Instruction::Halt() { return {.op = OpCode::HALT}; }
+
+Instruction Instruction::Jmp(JumpOffset offset) {
+  return {
+    .op = OpCode::JMP,
+    .parameters = {
+      .jmp = {
+	.offset = offset,
+      }
+    }
+  };
+}
+
+Instruction Instruction::JmpC(JumpOffset offset, RegisterIndex source) {
+  return {
+    .op = OpCode::JMPC,
+    .parameters = {
+      .jmpc = {
+	.offset = offset,
+	.source = source
+      }
+    }
+  };
+}
+
+Instruction Instruction::PrepareExec(RegisterCount count) {
+  return {
+    .op = OpCode::PREPARE_EXEC,
+    .parameters = {
+      .prepare = {
+	.count = count
+      }
+    }
+  };
+}
+
+Instruction Instruction::Arg(RegisterIndex target, RegisterIndex source) {
+  return {
+    .op = OpCode::ARG,
+    .parameters = {
+      .arg = {
+	.target = target,
+	.source = source
+      }
+    }
+  };
+}
+
+Instruction Instruction::Exec(RegisterIndex target, ProgramIndex entry) {
+  return {
+    .op = OpCode::EXEC,
+    .parameters = {
+      .exec = {
+	.target = target,
+	.entry = entry
+      }
+    }
+  };
+}
+
+Instruction Instruction::Ret(RegisterIndex source) {
+  return {
+    .op = OpCode::RET,
+    .parameters = {
+      .ret = {
+	.source = source
+      }
+    }
+  };
+}
