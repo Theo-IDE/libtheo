d461b2c8258094bbb01beb644e68eddae21c0ba1
Peter Preinesberger <peter.preines@gmail.com>
2024-08-06 14:20:58 +0200

    feat: CFG Grammar Class

---
+++ Compiler/include/ParserGenerator/grammar.hpp
@@ -0,0 +1,79 @@
+#ifndef __LIBTHEO_C_PARSER_GENERATOR_GRAMMAR_HPP_
+#define __LIBTHEO_C_PARSER_GENERATOR_GRAMMAR_HPP_
+
+#include <utility>
+#include <vector>
+#include <set>
+#include <map>
+namespace Theo {
+
+  struct Grammar {
+
+    struct Symbol {
+      enum Type {
+	EPSILON = 0,
+	TERMINAL = 1,
+	NON_TERMINAL = 2
+      };
+      Type t;
+      unsigned int index;
+
+      static Symbol Epsilon();
+      static Symbol Terminal(unsigned int index);
+    };
+
+    typedef std::vector<Symbol> Alternative; // one alternative of a right side
+    
+    unsigned int total_non_terminals;
+    std::map<Symbol, std::vector<Alternative>> right_sides; // index is non_terminal, value is all the alternatives for that symbol
+
+    std::map<Symbol, std::set<Symbol>> first_sets;
+
+    Grammar() : total_non_terminals(0), right_sides({}) {};
+
+    /**
+     * create a new ton-terminal symbol (a new left side)
+     */
+    Grammar::Symbol createNonTerminal();
+
+    void calculateFirstSets();
+    
+    /**
+     * compute the set of terminal symbols a string of grammar symbols might start with;
+     */
+    std::set<Grammar::Symbol> first(std::vector<Grammar::Symbol> string);
+  };
+
+  bool operator<(const Grammar::Symbol& g1, const Grammar::Symbol& g2);
+
+  /* symbol concatenation */
+  Grammar::Alternative operator,(const Grammar::Symbol left, const Grammar::Symbol right);     
+  Grammar::Alternative operator,(Grammar::Alternative left, const Grammar::Symbol right);
+
+  /* rule construction */
+  std::pair<Grammar::Symbol, Grammar::Alternative>
+  operator>>(const Grammar::Symbol left, const Grammar::Alternative right);
+  
+  template <typename SemanticType>
+  struct SemanticGrammar : public Grammar {
+    using Action = SemanticType (*)(std::vector<SemanticType>);
+    using RightSideAction = std::vector<Action>;
+    std::map<Symbol, RightSideAction> actions;
+
+    SemanticGrammar() : Grammar() {};
+
+    void add(std::pair<Symbol, std::vector<Symbol>> rule, Action a) {
+      if (rule.first.t != Symbol::NON_TERMINAL)
+	return;
+      if (!right_sides.contains(rule.first)) {
+	right_sides.insert(std::make_pair(rule.first, std::vector<Alternative>{rule.second}));
+	actions.insert(std::make_pair(rule.first, std::vector<Action>{a}));
+	return;
+      }
+      right_sides[rule.first].push_back(rule.second);
+      actions[rule.first].push_back(a);
+    }
+  };
+};
+
+#endif
---
+++ Compiler/src/ParserGenerator/grammar.cpp
@@ -0,0 +1,129 @@
+#include "Compiler/include/ParserGenerator/grammar.hpp"
+
+using namespace Theo;
+
+bool Theo::operator<(const Grammar::Symbol &g1,
+	       const Grammar::Symbol &g2) {
+  if (g1.t < g2.t)
+    return true;
+  if (g1.t > g2.t)
+    return false;
+  return g1.index < g2.index;
+}
+
+std::vector<Grammar::Symbol>
+Theo::operator,(const Grammar::Symbol left,
+          const Grammar::Symbol right) {
+  return {left, right};
+}
+
+std::vector<Grammar::Symbol>
+Theo::operator,(std::vector<Grammar::Symbol> left,
+          const Grammar::Symbol right) {
+  left.push_back(right);
+  return left;
+}
+
+std::pair<Grammar::Symbol, std::vector<Grammar::Symbol>>
+Theo::operator>>(const Grammar::Symbol left,
+	   const std::vector<Grammar::Symbol> right) {
+  return std::make_pair(left, right);
+}
+
+Grammar::Symbol Grammar::createNonTerminal() {
+  Symbol s = {Symbol::NON_TERMINAL, total_non_terminals};
+  total_non_terminals++;
+  return s;
+}
+
+Grammar::Symbol Theo::Grammar::Symbol::Epsilon() {
+  return {Grammar::Symbol::EPSILON, 0};
+}
+
+Grammar::Symbol Theo::Grammar::Symbol::Terminal(unsigned int index) {
+  return {Grammar::Symbol::TERMINAL, index};
+}
+
+void add_terminals(std::set<Grammar::Symbol> &to,
+                   std::set<Grammar::Symbol> &from) {
+  for(auto &f : from) {
+    if (f.t != Grammar::Symbol::TERMINAL)
+      continue;
+    to.insert(f);
+  }
+}
+
+void Grammar::calculateFirstSets() {
+  bool changed = true;
+  first_sets.insert(std::make_pair(Symbol::Epsilon(), std::set<Symbol>{}));
+  while(changed) { // repeat until nothing changes:
+    changed = false;
+    // 1) if X is a terminal, FIRST(X) = {X}
+    // 2) if X -> ø exists, FIRST(X) = FIRST(X) v {ø}
+    for (auto &rule : right_sides) {
+      Symbol left = rule.first;
+      for (auto &alternative : rule.second) {
+	Symbol first = alternative[0];
+	if (first.t == Symbol::EPSILON) { // 2
+	  if (!first_sets.contains(left)) {
+	    changed = true;
+	    first_sets.insert(std::make_pair(left, std::set<Symbol>{first}));
+	  } else if (!first_sets[left].contains(first)) {
+	    changed = true;
+	    first_sets[left].insert(first);
+	  }
+	}
+	for (auto &symbol : alternative) {
+	  if (symbol.t != Symbol::TERMINAL){ // 1
+	    if (symbol.t == Symbol::NON_TERMINAL && !first_sets.contains(symbol))
+	      first_sets.insert(std::make_pair(symbol, std::set<Symbol>{}));
+	    continue;
+	  }
+	  if (!first_sets.contains(symbol)) {
+	    changed = true;
+	    first_sets.insert(std::make_pair(symbol, std::set<Symbol>{symbol}));
+	  }
+	}
+      }
+    }
+    // 3) X -> Y_1Y_2Y_3 ..., FIRST(X) = FIRST(Y_1), if ø in FIRST(Y_1), then FIRST(X) = FIRST(X) v FIRST(Y_2) ...
+    for(auto &rule : right_sides) {
+      Symbol left = rule.first;
+      if (!first_sets.contains(left))
+	first_sets.insert(std::make_pair(left, std::set<Symbol>{}));
+      for (auto &alternative : rule.second) {
+	bool all_contain_epsilons = true;
+	for (auto &symbol : alternative) {
+	  int size_before = first_sets[left].size();
+	  std::set<Symbol>& sset = first_sets[symbol];
+	  add_terminals(first_sets[left], sset);
+	  if (first_sets[left].size() > size_before)
+	    changed = true;
+	  if (!sset.contains(Symbol::Epsilon())){
+	    all_contain_epsilons = false;
+	    break;
+	  }
+	}
+	if (all_contain_epsilons && !first_sets[left].contains(Symbol::Epsilon())){
+	  changed = true;
+	  first_sets[left].insert(Symbol::Epsilon());
+	}
+      }
+    }
+  }
+}
+
+std::set<Grammar::Symbol> Grammar::first(std::vector<Grammar::Symbol> string) {
+  std::set<Symbol> result = {};
+  bool all_epsilons = true;
+  for (auto &sym : string) {
+    add_terminals(result, first_sets[sym]);
+    if (!first_sets[sym].contains(Symbol::Epsilon())) {
+      all_epsilons = false;
+      break;
+    }
+  }
+  if (all_epsilons)
+    result.insert(Symbol::Epsilon());
+  return result;
+}
